afd2b2721660a7238f70718dce72789f801f8a45
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2024-04-23 15:07:39 +0300

    fun for nerds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { WordInputComponent } from './word-input/word-input.component';
+import { OddLengthComponent } from './odd-length/odd-length.component';
+import { EvenLengthComponent } from './even-length/even-length.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [WordInputComponent, OddLengthComponent, EvenLengthComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'fun-for-nerds';
+  partialOddWord: string = '';
+  partialEvenWord: string = '';
+
+  oddLengthWords: string[] = [];
+  evenLengthWords: string[] = [];
+
+  onWord(word: string) {
+    if (word.length % 2 === 0) {
+      this.evenLengthWords.push(word);
+    } else {
+      this.oddLengthWords.push(word);
+    }
+  }
+
+  onPartialWord(word: string) {
+    if (word.length % 2 === 0) {
+      this.partialEvenWord = word;
+      this.partialOddWord = '';
+    } else {
+      this.partialOddWord = word;
+      this.partialEvenWord = '';
+    }
+  }
 }
